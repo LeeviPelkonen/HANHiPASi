87c78437e4dfe26d4545da4f3b6016c2afe5caab
visaso <32449867+visaso@users.noreply.github.com>
2019-11-28 22:43:40 +0200

    Questionnaire changes, merge manually later

---
+++ app/src/main/java/com/example/arkki/Questionnaire/WordFragment.kt
@@ -0,0 +1,110 @@
+package com.example.arkki.Questionnaire
+
+import android.content.Context
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.LinearLayout
+import androidx.core.os.postDelayed
+import androidx.fragment.app.Fragment
+import com.example.arkki.OnFragmentInteractionListener
+import com.example.arkki.R
+import kotlinx.android.synthetic.main.fragment_word.view.*
+
+
+class WordFragment(private val question: Question) : Fragment() {
+    private var listener: OnFragmentInteractionListener? = null
+    private lateinit var inAnimation: Animation
+    private lateinit var outAnimation: Animation
+    private var buttonList = mutableListOf<LinearLayout>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_word, container, false)
+        // Initialize texts
+        view.questionTitle.text = question.question
+        view.textA.text = question.answers[0]
+        view.textB.text = question.answers[1]
+        view.textC.text = question.answers[2]
+        view.textD.text = question.answers[3]
+        buttonList.add(view.answerA)
+        buttonList.add(view.answerB)
+        buttonList.add(view.answerC)
+        buttonList.add(view.answerD)
+
+        setupClickListeners(view)
+        setupAnimator()
+        //outAnimation(view)
+
+        return view
+    }
+
+    private fun setupClickListeners(v: View) {
+        v.answerA.setOnClickListener {
+            outAnimation()
+            Handler().postDelayed(500) {
+                onButtonPressed(question.correctAnswer(v.textA.text.toString()))
+            }
+        }
+        v.answerB.setOnClickListener {
+            outAnimation()
+            Handler().postDelayed(500) {
+                onButtonPressed(question.correctAnswer(v.textB.text.toString()))
+            }
+        }
+        v.answerC.setOnClickListener {
+            outAnimation()
+            Handler().postDelayed(500) {
+                onButtonPressed(question.correctAnswer(v.textC.text.toString()))
+            }
+        }
+        v.answerD.setOnClickListener {
+            outAnimation()
+            Handler().postDelayed(500) {
+                onButtonPressed(question.correctAnswer(v.textD.text.toString()))
+            }
+        }
+    }
+
+    private fun setupAnimator() {
+        inAnimation = AnimationUtils.loadAnimation(context, R.anim.slide_left_to_right)
+        buttonList.forEach {
+            it.visibility = View.VISIBLE
+            it.startAnimation(inAnimation)
+        }
+    }
+
+    private fun outAnimation() {
+        outAnimation = AnimationUtils.loadAnimation(context, R.anim.slide_away_right)
+        buttonList.forEach {
+            it.startAnimation(outAnimation)
+        }
+    }
+
+    private fun onButtonPressed(correct: Boolean) {
+        listener?.onFragmentInteraction(correct)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        }
+
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/arkki/QuestionnaireEndActivity.kt
@@ -0,0 +1,36 @@
+package com.example.arkki
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_questionnaire_end.*
+
+class QuestionnaireEndActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questionnaire_end)
+
+        val maxScore = intent.getIntExtra("MaxScore", 0)
+        val score = intent.getIntExtra("Score",0)
+        val percentage = (((score).toDouble() / (maxScore).toDouble()) * 100).toInt()
+
+        if (percentage < 50 ) {
+            congratulationsLabel.text = getString(R.string.pahus, percentage)
+        } else {
+            redeemReward.visibility = View.VISIBLE
+            congratulationsLabel.text = getString(R.string.jippii, percentage)
+        }
+
+        redeemReward.setOnClickListener {
+
+        }
+
+        playAgainQuiz.setOnClickListener {
+            val i = Intent(this, QuestionnaireActivity::class.java)
+            startActivity(i)
+        }
+
+    }
+}
